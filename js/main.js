7c4c66ed6ad279f9a11a7f1e619c5765c4b19f8f
andre-devito <andre_devito@hotmail.com>
2017-12-14 16:59:14 -0200

    v1.1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Cadastro de Pacientes";
+// titulo.addEventListener("click", mostrarMensagem);
 
+var botaoAdicionar = document.querySelector("#botao-paciente");
+botaoAdicionar.addEventListener("click", mostrarMensagem);
+
+	// Teste
 	var titulo = document.querySelector("h1");
 	console.log(titulo.classList);
 
@@ -33,4 +38,40 @@ for (var i = 0; i < pacientes.length; i++) {
 	
 }
 
+function mostrarMensagem(event){
+	event.preventDefault();
+	
+	var tabela = document.querySelector("#tabela-pacientes");
+	var form = document.querySelector("#form-adicionar");
+	
+	var nome = form.nome.value;
+	var peso = form.peso.value;
+	var altura = form.altura.value;
+	var gordura = form.gordura.value;
+	var imc = 0;
+	
+	var pacienteTr = document.createElement("tr");
+	
+	var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");	
+	
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+	imcTd.textContent = imc.toFixed(2);
+	
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+	pacienteTr.appendChild(imcTd);
+	
+	tabela.appendChild(pacienteTr);
+	
+	window.alert("click ok!!!");
+}
 
